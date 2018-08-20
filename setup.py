409411f5c71dafcd0c1c46fc4e4981da5d19db40
Donald Hsu <donald.hsu@moxa.com>
2018-08-20 17:39:06 +0800

    0.1.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*paths):
 
 setuptools.setup(
     name="sservice",
-    version="0.1.2",
+    version="0.1.3",
     author="Sanji Team",
     author_email="sanji@moxa.com",
     description="Systemd Service Wrapper",
